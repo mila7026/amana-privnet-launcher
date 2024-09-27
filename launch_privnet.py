7ea7525f36ad16011cab42da6d0f21d1b5c3b355
jahali6128 <jahali6128@gmail.com>
2024-09-27 01:15:38 +0100

    Custom genesis & auto bootnode

--- launch_privnet.py
+++ launch_privnet.py
@@ -4,24 +4,41 @@ import secrets
 import json
 import sys
 import subprocess
+from ellipticcurve.privateKey import PrivateKey
+
 
 # We will assume "qng" directory to be located in users home directory 
 QNG_DIRECTORY = os.path.expanduser("~") + "/qng_privnet"
 NODE_1_DIRECTORY =  QNG_DIRECTORY + "/node1"
 NODE_2_DIRECTORY =  QNG_DIRECTORY + "/node2"
 NODE_3_DIRECTORY =  QNG_DIRECTORY + "/node3"
-GENESIS_SOURCE_CODE = os.path.expanduser("~") + "/qng/meerevm/amana/genesis.go"
 QNG_BINARY = os.path.expanduser("~") + "/qng/build/bin/qng"
 
 PRIV_KEY_1 = "0x" + secrets.token_hex(32)  # generate 256 bit (32 bytes) private key
 PRIV_KEY_2 = "0x" + secrets.token_hex(32)  
 PRIV_KEY_3 = "0x" + secrets.token_hex(32)  
 
+# Customise as necessary - Change these if they will be remote nodes
+IP_ADDRESS_1 = "127.0.0.1"
+IP_ADDRESS_2 = "127.0.0.1"
+IP_ADDRESS_3 = "127.0.0.1"
+
+PORT_1 = 30303
+PORT_2 = 30303
+PORT_3 = 30303
+
+# Generate enode private key (ECDSA 128 bit) for each node
+ENODE_PREFIX = "enode://"
+# ENODE_1 = ENODE_PREFIX + PrivateKey().publicKey().toString() + "@" + IP_ADDRESS_1 + ":30303"
+ENODE_PRIV_1 = PrivateKey()
+ENODE_PRIV_2 = PrivateKey()
+ENODE_PRIV_3 = PrivateKey()
+
+
 def create_directories():
     # Lets create some directories to host our files
     try:
         os.mkdir(QNG_DIRECTORY)    # First our parent directory
-
         # Now our sub-directories : node {1,2,3}
         os.mkdir(NODE_1_DIRECTORY)
         os.mkdir(NODE_2_DIRECTORY)
@@ -32,19 +49,22 @@ def create_directories():
         sys.exit(1)
 
 def generate_priv_keys():
-    # Lets store our keys and addresses inside the file "private_keys.json" file    
+    # Lets store our keys, addresses & enode address inside the file "private_keys.json" file    
     priv_json = {
         "node_1": {
             "private_key": PRIV_KEY_1,
-            "address": Account.from_key(PRIV_KEY_1).address
+            "address": Account.from_key(PRIV_KEY_1).address,
+            "enode": ENODE_PREFIX + ENODE_PRIV_1.publicKey().toString() + "@" + IP_ADDRESS_1 + ":" + str(PORT_1)
         },
         "node_2": {
             "private_key": PRIV_KEY_2,
-            "address": Account.from_key(PRIV_KEY_2).address
+            "address": Account.from_key(PRIV_KEY_2).address,
+            "enode": ENODE_PREFIX + ENODE_PRIV_2.publicKey().toString() + "@" + IP_ADDRESS_2 + ":" + str(PORT_2)
         },
         "node_3": {
             "private_key": PRIV_KEY_3,
-            "address": Account.from_key(PRIV_KEY_3).address
+            "address": Account.from_key(PRIV_KEY_3).address,
+            "enode": ENODE_PREFIX + ENODE_PRIV_3.publicKey().toString() + "@" + IP_ADDRESS_3 + ":" + str(PORT_3)
         }
     }
 
@@ -64,32 +84,22 @@ def gen_binary():
     sealer_addr_3 = Account.from_key(PRIV_KEY_3).address[2:]
 
     sig = "0" * 130     # 65 bytes
+    # Edit this to decide who becomes a sealer
     extraData = "0x" + vanity + sealer_addr_1 + sealer_addr_2 + sealer_addr_3 + sig  
 
-    print("Modifying genesis.go...")
-
-    if os.stat(GENESIS_SOURCE_CODE).st_size == 0:
-        print("Genesis source file cannot be empty!")
-        sys.exit(1)
+    # Modiy custom_amana.json file
+    with open("custom_amana.json", "rw") as json_file:
+        amana_gen = json.load(json_file)
+        amana_gen["extraData"] = extraData
         
-    custom_genesis = ""
+        # allocate some funds to specified accounts 
+        # e.g. amana_gen["alloc"] = {"0x123ABC....": {"balance": "0x33b2e3c9fd0803ce8000000"}, ...}        
+        amana_gen["alloc"] = {
+            Account.from_key(PRIV_KEY_1).address: {"balance": "0x33b2e3c9fd0803ce8000000"}
+        }
+        # Write new json object to file
+        json_file.write(json.dumps(amana_gen))
 
-    with open("genesis.go", "r") as genesis_file:
-
-        read_content = genesis_file.read()
-        repl_extra_data = read_content.replace("0x000000000000000000000000000000000000000000000000000000000000000071bc4403af41634cda7c32600a8024d54e7f64990000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-                                 extraData)
-        repl_alloc_ether = repl_extra_data.replace("0x71bc4403af41634cda7c32600a8024d54e7f6499", Account.from_key(PRIV_KEY_1).address) 
-        custom_genesis = repl_alloc_ether
-
-    #  # We can use custom genesis now - this is no longer needed
-    
-    # with open(GENESIS_SOURCE_CODE, "w") as orig_binary:
-    #     orig_binary.write(custom_genesis)     # replace genesis.go with our modified version
-        
-    # Lets compile our new binary from source code
-    # print ("Compiling qng binary...")
-    # subprocess.Popen("cd " + os.path.expanduser("~") + "/qng && make", shell=True).wait()
 
 def create_node_files():
     # Copy binary into eacb one of the nodes
@@ -101,7 +111,18 @@ def create_node_files():
     subprocess.Popen(NODE_1_DIRECTORY + "/qng --privnet -A " + NODE_1_DIRECTORY + " --amana --cleanup", shell=True).wait()
     subprocess.Popen(NODE_2_DIRECTORY + "/qng --privnet -A " + NODE_2_DIRECTORY + " --amana --cleanup", shell=True).wait()
     subprocess.Popen(NODE_3_DIRECTORY + "/qng --privnet -A " + NODE_3_DIRECTORY + " --amana --cleanup", shell=True).wait()
-            
+    
+      # Add the network key to each node
+    with open(NODE_1_DIRECTORY + "/data/privnet/network.key", "w") as network_key_file:
+        network_key_file.write(ENODE_PRIV_1.toString())
+
+    with open(NODE_2_DIRECTORY + "/data/privnet/network.key", "w") as network_key_file:
+        network_key_file.write(ENODE_PRIV_2.toString())
+    
+    with open(NODE_3_DIRECTORY + "/data/privnet/network.key", "w") as network_key_file:
+        network_key_file.write(ENODE_PRIV_3.toString())
+    
+     
 def keystore():
     print("Generating keystore files for each node...")
 
@@ -131,25 +152,34 @@ def keystore():
     subprocess.Popen("cp password.txt " + NODE_1_DIRECTORY, shell=True).wait()
     subprocess.Popen("cp password.txt " + NODE_2_DIRECTORY, shell=True).wait()
     subprocess.Popen("cp password.txt " + NODE_3_DIRECTORY, shell=True).wait()
-
-
-def gen_config(config_num:int) -> str:
-    # num -> ETH address
-    num_to_address = {1: Account.from_key(PRIV_KEY_1).address,
-                      2: Account.from_key(PRIV_KEY_2).address,
-                      3: Account.from_key(PRIV_KEY_3).address}
     
-    num_to_port = {1: 38528, 2:38529, 3: 38530}
-    http_port = {1: 8545, 2: 8546, 3: 8547}
+
+def gen_config(config_num: int) -> str:
+    # num -> ETH address
+    num_to_address = { 1: Account.from_key(PRIV_KEY_1).address,
+                       2: Account.from_key(PRIV_KEY_2).address,
+                       3: Account.from_key(PRIV_KEY_3).address }
+    
+    num_to_port = { 1: PORT_1, 2: PORT_2, 3: PORT_3 }
+    # http_port = {1: 8545, 2: 8546, 3: 8547}
+    enode_addrs = { 1: ENODE_PRIV_1.publicKey().toString() + "@" + IP_ADDRESS_1 + ":" + str(PORT_1),
+                    2: ENODE_PRIV_2.publicKey().toString() + "@" + IP_ADDRESS_2 + ":" + str(PORT_2),
+                    3: ENODE_PRIV_3.publicKey().toString() + "@" + IP_ADDRESS_3 + ":" + str(PORT_3) }
+    
+    bootnodeToConfig = { 1: enode_addrs[2] + "," + enode_addrs[3], 
+                         2: enode_addrs[1] + "," + enode_addrs[3], 
+                         3: enode_addrs[1] + "+" + enode_addrs[2] }
     
     # Weird formatting but must be kept like this - do not alter!
-    # We will disable RPC as this is not needed if solely using Amana
+    # We will disable blockDAG RPC as this is not needed if solely using Amana
+    # Update - disabled the RPC server for qng EVM
 
     config_text = f'''privnet=true
 amana=true
-amanaenv="--unlock {num_to_address[config_num]} --password password.txt --port {num_to_port[config_num]} --miner.pending.feeRecipient {num_to_address[config_num]} --mine"
+amanaenv="--unlock { num_to_address[config_num] } --password password.txt --port { num_to_port[config_num] } --bootnodes { bootnodeToConfig[config_num] } --miner.pending.feeRecipient { num_to_address[config_num] } --mine"
+amangen="custom_amana.json"
 norpc=true
-
+evmenv="--nodiscover --networkid 0 --port 0"
 
     '''
     return config_text
@@ -168,6 +198,7 @@ def create_config():
     with open(NODE_3_DIRECTORY + "/config_3.toml", "w") as config_file:
         config_text = gen_config(3)
         config_file.write(config_text)
+
 
 def main():
     create_directories()
